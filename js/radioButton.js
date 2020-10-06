0e2f7af3b20447df02727e6ce5deccdb68a9013e
PovilasZubrys <48654760+PovilasZubrys@users.noreply.github.com>
2020-10-06 22:15:02 +0300

    changes

--- js/radioButton.js
+++ js/radioButton.js
@@ -1,20 +1,40 @@
 function selectOnlyThis(id) {
-    for (var i = 1;i <= 3; i++) {
+    const setBackground = document.querySelectorAll('.container');
+    const button = document.getElementById('cartButton');
+    for (var i = 0; i < setBackground.length; i++){
         var mygt = document.getElementById("Check" + i).checked = false;
-    }
+        if (id === 'Check0') {
+            button.setAttribute("href", "http://www.facebook.com");
+            console.log(button);
+            setBackground[0].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+            setBackground[1].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+            setBackground[2].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+        } else if (id === 'Check1') {
+            button.setAttribute("href", "http://www.reddit.com");
+            console.log(button);
+            setBackground[0].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+            setBackground[1].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+            setBackground[2].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+        } else if (id === 'Check2') {
+            button.setAttribute("href", "http://www.google.com");
+            console.log(button);
+            setBackground[0].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+            setBackground[1].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+            setBackground[2].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
+        }
         document.getElementById(id).checked = true;
+        console.log(button);
     }
-function paspaudimas(){       
-    if(document.getElementById('Check1').checked === true){
-        console.log('Pirmasis!!!');
-        window.open("http://www.google.lt","_self") //option 1 linkas
-    } else if(document.getElementById('Check2').checked === true) {
-        console.log('Antrasis!!!');
-        window.open("http://www.facebook.com","_self") //option 2 linkas
-    } else if(document.getElementById('Check3').checked === true) {
-        console.log('Trečiasis!!!');
-        window.open("http://www.reddit.com","_self") //option 3 linkas
-    } else {
-        console.log('Nepasirinktas');
-    }
+
 }
+// function paspaudimas(){
+//     if(document.getElementById('Check1').checked === true){
+//         console.log('Pirmasis!!!');
+//     } else if(document.getElementById('Check2').checked === true) {
+//         console.log('Antrasis!!!');
+//     } else if(document.getElementById('Check3').checked === true) {
+//         console.log('Trečiasis!!!');
+//     } else {
+//         console.log('Nepasirinktas');
+//     }
+// }
