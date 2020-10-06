982dd85e158a47d174d358750919fc3fd67f9d6f
PovilasZubrys <48654760+PovilasZubrys@users.noreply.github.com>
2020-10-06 23:02:21 +0300

    changes

--- js/radioButton.js
+++ js/radioButton.js
@@ -1,40 +1,34 @@
 function selectOnlyThis(id) {
+    
     const setBackground = document.querySelectorAll('.container');
     const button = document.getElementById('cartButton');
+    var price = document.querySelector("span.price")
+    var oldPrice = document.querySelector("span.oldPrice")
+
     for (var i = 0; i < setBackground.length; i++){
         var mygt = document.getElementById("Check" + i).checked = false;
         if (id === 'Check0') {
-            button.setAttribute("href", "http://www.facebook.com");
-            console.log(button);
+            button.setAttribute("href", "http://www.facebook.com"); // 1 Linkas kurį kels į HTML
+            oldPrice.textContent = "55.45$" // Sena kaina
+            price.textContent = "35.95$" // Nauja kaina
             setBackground[0].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[1].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[2].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check1') {
-            button.setAttribute("href", "http://www.reddit.com");
-            console.log(button);
+            button.setAttribute("href", "http://www.reddit.com"); // 2 Linkas kurį kels į HTML
+            oldPrice.textContent = "145.65$" // Sena kaina
+            price.textContent = "99.99$" // Nauja kaina
             setBackground[0].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[1].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[2].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check2') {
-            button.setAttribute("href", "http://www.google.com");
-            console.log(button);
+            button.setAttribute("href", "http://www.google.com"); // 3 Linkas kurį kels į HTML
+            oldPrice.textContent = "195.55" // Sena kaina
+            price.textContent = "150.00$" // Nauja kaina
             setBackground[0].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[1].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[2].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         }
         document.getElementById(id).checked = true;
-        console.log(button);
     }
-
 }
-// function paspaudimas(){
-//     if(document.getElementById('Check1').checked === true){
-//         console.log('Pirmasis!!!');
-//     } else if(document.getElementById('Check2').checked === true) {
-//         console.log('Antrasis!!!');
-//     } else if(document.getElementById('Check3').checked === true) {
-//         console.log('Trečiasis!!!');
-//     } else {
-//         console.log('Nepasirinktas');
-//     }
-// }
