676ee7974ef587276bc3abf9d959c119b387e485
PovilasZubrys <48654760+PovilasZubrys@users.noreply.github.com>
2020-10-07 09:34:02 +0300

    changes

--- js/radioButton.js
+++ js/radioButton.js
@@ -11,6 +11,7 @@ function selectOnlyThis(id) {
             button.setAttribute("href", "http://www.facebook.com"); // 1 Linkas kurį kels į HTML
             oldPrice.textContent = "55.45$" // Sena kaina
             price.textContent = "35.95$" // Nauja kaina
+            // keičia radio pasirinkimo fonus
             setBackground[0].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[1].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[2].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
@@ -18,6 +19,7 @@ function selectOnlyThis(id) {
             button.setAttribute("href", "http://www.reddit.com"); // 2 Linkas kurį kels į HTML
             oldPrice.textContent = "145.65$" // Sena kaina
             price.textContent = "99.99$" // Nauja kaina
+            // keičia radio pasirinkimo fonus
             setBackground[0].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[1].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[2].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
@@ -25,6 +27,7 @@ function selectOnlyThis(id) {
             button.setAttribute("href", "http://www.google.com"); // 3 Linkas kurį kels į HTML
             oldPrice.textContent = "195.55" // Sena kaina
             price.textContent = "150.00$" // Nauja kaina
+            // keičia radio pasirinkimo fonus
             setBackground[0].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[1].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[2].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
