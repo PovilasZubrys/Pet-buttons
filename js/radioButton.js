7ff75f166bc10de95b417916800e9d4a04b7a8ed
PovilasZubrys <48654760+PovilasZubrys@users.noreply.github.com>
2020-10-14 11:52:49 +0300

    JS changes

--- js/radioButton.js
+++ js/radioButton.js
@@ -9,7 +9,7 @@ function selectOnlyThis(id) {
     for (var i = 0; i < setBackground.length; i++){
         var mygt = document.getElementById("Check" + i).checked = false;
         if (id === 'Check0') {
-            buttonOneTime.setAttribute("href", "http://www.15min.lt"); // 1 Linkas kurį kels į HTML
+            buttonOneTime.setAttribute("href", "http://www.15min.lt"); // 1 Linkas kurį kels į HTML (OneTimePurchase)
             oldPrice.textContent = "55.45$" // Sena kaina
             price.textContent = "35.95$" // Nauja kaina
             // keičia radio pasirinkimo fonus
@@ -20,7 +20,7 @@ function selectOnlyThis(id) {
             setBackground[4].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[5].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check1') {
-            buttonOneTime.setAttribute("href", "http://www.delfi.lt"); // 2 Linkas kurį kels į HTML
+            buttonOneTime.setAttribute("href", "http://www.delfi.lt"); // 2 Linkas kurį kels į HTML (OneTimePurchase)
             oldPrice.textContent = "145.65$" // Sena kaina
             price.textContent = "99.99$" // Nauja kaina
             // keičia radio pasirinkimo fonus
@@ -31,7 +31,7 @@ function selectOnlyThis(id) {
             setBackground[4].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[5].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check2') {
-            buttonOneTime.setAttribute("href", "http://www.lrytas.lt"); // 3 Linkas kurį kels į HTML
+            buttonOneTime.setAttribute("href", "http://www.lrytas.lt"); // 3 Linkas kurį kels į HTML (OneTimePurchase)
             oldPrice.textContent = "195.55" // Sena kaina
             price.textContent = "150.00$" // Nauja kaina
             // keičia radio pasirinkimo fonus
@@ -42,7 +42,7 @@ function selectOnlyThis(id) {
             setBackground[4].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[5].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check3') {
-            buttonSubscription.setAttribute("href", "http://www.facebook.com"); // 1 Linkas kurį kels į HTML
+            buttonSubscription.setAttribute("href", "http://www.facebook.com"); // 1 Linkas kurį kels į HTML (Subscription)
             oldPrice.textContent = "55.45$" // Sena kaina
             price.textContent = "35.95$" // Nauja kaina
             // keičia radio pasirinkimo fonus
@@ -53,7 +53,7 @@ function selectOnlyThis(id) {
             setBackground[4].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[5].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check4') {
-            buttonSubscription.setAttribute("href", "http://www.reddit.com"); // 2 Linkas kurį kels į HTML
+            buttonSubscription.setAttribute("href", "http://www.reddit.com"); // 2 Linkas kurį kels į HTML (Subscription)
             oldPrice.textContent = "145.65$" // Sena kaina
             price.textContent = "99.99$" // Nauja kaina
             // keičia radio pasirinkimo fonus
@@ -64,7 +64,7 @@ function selectOnlyThis(id) {
             setBackground[4].setAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
             setBackground[5].removeAttribute("style", "background-color: #D26546; border-radius: 10px; color: white;");
         } else if (id === 'Check5') {
-            buttonSubscription.setAttribute("href", "http://www.google.com"); // 3 Linkas kurį kels į HTML
+            buttonSubscription.setAttribute("href", "http://www.google.com"); // 3 Linkas kurį kels į HTML (Subscription)
             oldPrice.textContent = "195.55" // Sena kaina
             price.textContent = "150.00$" // Nauja kaina
             // keičia radio pasirinkimo fonus
